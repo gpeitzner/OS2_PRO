35faa5670ee969243b76e8e18ce8a37c6cdc68b3
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-12-27 22:22:16 -0600

    [add] web deployment script

--- gke.sh
+++ gke.sh
@@ -5,3 +5,11 @@ docker push gcr.io/$DEVSHELL_PROJECT_ID/api:1.0
 kubectl create namespace api
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
+
+docker build -t gcr.io/$DEVSHELL_PROJECT_ID/web:1.0 .
+gcloud auth configure-docker
+docker push gcr.io/$DEVSHELL_PROJECT_ID/web:1.0
+
+kubectl create namespace web
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
