1265bb1859492c76c87582585ad37af5605b250e
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-12-27 21:52:44 -0600

    [add] games catalog

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
+import { Game } from '../game';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,8 @@ import { DataService } from '../data.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  games: Game[];
+
   constructor(
     public dataService: DataService,
     private httpClient: HttpClient,
@@ -17,6 +20,12 @@ export class HomeComponent implements OnInit {
     if (!this.dataService.user) {
       this.router.navigateByUrl('/');
     }
+    this.httpClient.get('http://104.154.97.177:8080/games').subscribe(
+      (data: Game[]) => {
+        this.games = data;
+      },
+      () => {}
+    );
   }
 
   ngOnInit(): void {}
