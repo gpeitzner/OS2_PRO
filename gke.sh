9fb322fac027ef2a5f3cc569f6936e712f1d3a8a
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-12-27 22:30:36 -0600

    [add] database deploy script

--- gke.sh
+++ gke.sh
@@ -1,15 +1,18 @@
+#database
+kubectl create namespace database
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+#api
 docker build -t gcr.io/$DEVSHELL_PROJECT_ID/api:1.0 .
 gcloud auth configure-docker
 docker push gcr.io/$DEVSHELL_PROJECT_ID/api:1.0
-
 kubectl create namespace api
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-
+#web
 docker build -t gcr.io/$DEVSHELL_PROJECT_ID/web:1.0 .
 gcloud auth configure-docker
 docker push gcr.io/$DEVSHELL_PROJECT_ID/web:1.0
-
 kubectl create namespace web
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
